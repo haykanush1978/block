d538734acdfd6602d1c49d5314281151374d717f
haykanush1978 <32386049+haykanush1978@users.noreply.github.com>
2017-10-02 13:15:37 +0300

    Create 52.js

---
+++ 52.js
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.0;
+ 
+contract BusinessCard {
+    
+    mapping (bytes32 => string) data;
+    
+    function setData(string key, string value) {
+        data[sha3(key)] = value;
+    }
+    
+    function getData(string key) constant returns(string) {
+        return data[sha3(key)];
+    }
+ 
+}
